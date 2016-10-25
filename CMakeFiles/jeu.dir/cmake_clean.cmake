cd487a0e874be333cc9dc1cf1c92241d2ae745d9
Ali Bayrak <ali.bayrak@live.fr>
2016-10-25 15:46:48 +0200

    nouveau fichier : jeu 	modifié :         jeu.c 	nouveau fichier : resultat.txt

---
+++ CMakeFiles/jeu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jeu.dir/jeu.c.o"
+  "jeu.pdb"
+  "jeu"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jeu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- jeu.c
+++ jeu.c
@@ -1,21 +1,32 @@
-/*!\Fichier jeu.c
+/*!\file jeu.c
  *
- * \Exercice afin de mettre en place Emacs, GIT, Doxygen
+ * \brief Exercice afin de mettre en place Emacs, GIT, Doxygen
+ * \brief Ce programme permet de trouver une valeur compris entre 1 et 100   
  *
  * \author Ali Bayrak, ali.bayrak@live.fr
- * \date Octobre 25 2016
+ * \date October 25 2016
+ * \todo Creer un autre programme pour recuperer le meilleur score
+ * \todo       
  */
 
 //Appel des bibliotheques
 #include <stdio.h>
 #include <stdlib.h> 
+#include <time.h> 
+#include <string.h>
 
-//Main
+
+FILE* fichier = NULL;
+
+/*! \brief debut du main */
 int main (int argc, char *argv[]){
         // Definition des variables
         int valeur=0, nbessai=0, atrouver;
+        srand(time(NULL));
+        atrouver = rand() % 100; //initialisation de la valeur aléatoire
 
-        atrouver = rand() % 10; //initialisation de la valeur aléatoire
+        time_t t;
+        
         do{
                 printf("Entrer un nombre entre 1 et 100 : ");
                 scanf("%d", &valeur);
@@ -24,8 +35,17 @@ int main (int argc, char *argv[]){
                //verification
                if(atrouver > valeur) {printf("Plus haut\n");}
                else if(atrouver < valeur){printf("Plus bas\n");}
-               else printf("Bravo vous avez trouvé la bonne valeur en %d coups !\n",nbessai);
-        
+               else {
+                        printf("Bravo vous avez trouvé la bonne valeur en %d coups !\n",nbessai);
+                        time(&t);
+                        fichier = fopen("resultat.txt", "a+");
+                if (fichier != NULL){
+                fprintf(fichier, "%d coup(s) le %s", nbessai,ctime(&t));
+                fclose(fichier);
+                }
+                }
          }while(valeur != atrouver);
                  
 }
+
+
